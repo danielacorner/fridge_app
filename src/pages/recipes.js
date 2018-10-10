d7af03dd4bb26ac468265d4e074f08260f4931c5
dcorn068 <dcorn068@users.noreply.github.com>
2018-10-09 21:48:10 -0400

    switch recipes to class-based component, continue styling

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -3,138 +3,167 @@ import Layout from '../components/Layout';
 import styled from 'styled-components';
 import { Link, graphql } from 'gatsby';
 
-const Recipes = ({ data }) => {
-  const { edges: recipes } = data.allMarkdownRemark;
+class Recipes extends React.Component {
+  render() {
+    const { data } = this.props;
+    const { edges: recipes } = data.allMarkdownRemark;
 
-  const PageTitle = styled.h1`
-    display: grid;
-    width: 100%;
-    justify-items: center;
-    font-size: 36px;
-    text-decoration: underline;
-  `;
-
-  const Grid = styled.div`
-    display: grid;
-    grid-gap: 10px;
-    grid-template-columns: repeat(auto-fill, minmax(225px, 1fr));
-  `;
-
-  const RecipeCard = styled.div`
-    width: 100%;
-    max-height: 325px;
-    margin: 40px;
-    background: #eaeaea;
-    .card_contents {
-      padding: 15px;
-      width: 100%;
-      height: 100%;
+    const PageTitle = styled.h1`
       display: grid;
-      grid-gap: 5px;
-      grid-template-rows: 1fr auto auto;
-      grid-row: 1/-1;
-      grid-column: 1/-1;
-      .image_div {
-        overflow: hidden;
+      width: 100%;
+      justify-items: center;
+      font-size: 36px;
+      text-decoration: underline;
+    `;
+
+    const Grid = styled.div`
+      display: grid;
+      grid-gap: 10px;
+      grid-template-columns: repeat(auto-fill, minmax(225px, 1fr));
+    `;
+
+    const RecipeCard = styled.div`
+      width: 100%;
+      max-height: 325px;
+      margin: 40px;
+      background: #eaeaea;
+      .card_contents {
+        padding: 15px;
+        width: 100%;
+        height: 100%;
         display: grid;
-        min-height: 200px;
-        img {
-          grid-row: 1/-1;
-          grid-column: 1/-1;
-          height: 100%;
-          width: 100%;
-          object-fit: cover;
+        grid-gap: 5px;
+        grid-template-rows: 1fr auto auto;
+        grid-row: 1/-1;
+        grid-column: 1/-1;
+        .image_div {
+          overflow: hidden;
+          display: grid;
+          min-height: 200px;
+          img {
+            grid-row: 1/-1;
+            grid-column: 1/-1;
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+            transition: all 0.3s ease-out;
+          }
+          transition: all 0.3s ease-out;
         }
         transition: all 0.3s ease-out;
         &:hover {
-          box-shadow: 0px 3px 11px 1px #00000087;
-          .img_overlay {
-            transform: translateY(0);
+          box-shadow: 0px 3px 11px 1px rgba(0, 0, 0, 0.2);
+          .image_div {
+            box-shadow: 0px 3px 11px 1px #00000087;
+            img {
+              transform: scale(1.05);
+            }
+            .img_overlay {
+              transform: translateX(0);
+            }
+          }
+          h3::after {
+            transform: translateX(0);
           }
         }
-      }
-      h3 {
-        font-size: 20px;
-        font-style: italic;
-      }
-      p {
-        font-size: 14px;
-      }
-    }
-    .img_overlay {
-      grid-column: 1/-1;
-      grid-row: 1/-1;
-      width: 100%;
-      height: 100%;
-      display: grid;
-      justify-items: center;
-      align-content: center;
-      /* grid-template: 1/3; */
-      background: rgba(63, 72, 125, 0.6);
-      button,
-      a {
-        cursor: pointer;
-        background: none;
-        border: 2px solid rgba(255, 255, 255, 0.9);
-        color: white;
-        text-transform: uppercase;
-        background: (0, 0, 0, 0.7);
-        padding: 5px 10px;
-        margin: 5px 0;
-        font-size: 14px;
-        transition: all 0.2s ease-out;
-        &:hover {
-          box-shadow: 0px 3px 7px 1px #00000087;
+        overflow: hidden;
+        h3 {
+          font-size: 20px;
+          font-style: italic;
+          display: inline;
+          position: relative;
+          &::after {
+            content: '';
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+            transform: translateX(400%);
+            transition: all 0.5s cubic-bezier(0.74, 0.31, 0.13, 1.01);
+          }
+        }
+        p {
+          font-size: 14px;
         }
       }
-      transform: translateY(100%);
-      transition: all 0.3s ease-in-out;
-    }
-  `;
+      .img_overlay {
+        grid-column: 1/-1;
+        grid-row: 1/-1;
+        width: 100%;
+        height: 100%;
+        display: grid;
+        justify-items: center;
+        align-content: center;
+        /* grid-template: 1/3; */
+        background: rgba(63, 72, 125, 0.6);
+        button,
+        a {
+          cursor: pointer;
+          background: none;
+          border: 2px solid rgba(255, 255, 255, 0.9);
+          color: white;
+          text-transform: uppercase;
+          background: (0, 0, 0, 0.7);
+          padding: 5px 10px;
+          margin: 5px 0;
+          font-size: 14px;
+          transition: all 0.2s ease-out;
+          &:hover {
+            box-shadow: 0px 3px 7px 1px #00000087;
+          }
+        }
+        transform: translateX(-100%);
+        transition: all 0.3s ease-in-out;
+      }
+    `;
 
-  console.log(recipes);
-  return (
-    <Layout>
-      <PageTitle>Recipes!</PageTitle>
-      <Grid>
-        {recipes.map(rec => {
-          const { id } = rec.node;
-          const { title, image, description } = rec.node.frontmatter;
-          const maxTitleChars = 20;
-          const maxDescChars = 59;
-          return (
-            <RecipeCard key={id}>
-              <div className="card_contents">
-                <div className="image_div">
-                  <img src={image} alt={title} />
-                  <div className="img_overlay">
-                    <Link to={id}>View Recipe</Link>
-                    <button onClick={() => console.log('add')}>
-                      Add to Grocery List
-                    </button>
-                    <button onClick={() => console.log('save')}>
-                      Save for Later
-                    </button>
+    console.log(recipes);
+    return (
+      <Layout>
+        <PageTitle>Recipes!</PageTitle>
+        <Grid>
+          {recipes.map(rec => {
+            const { id } = rec.node;
+            const { title, image, description } = rec.node.frontmatter;
+            const maxTitleChars = 20;
+            const maxDescChars = 59;
+            return (
+              <RecipeCard key={id}>
+                <div className="card_contents">
+                  <div className="image_div">
+                    <img src={image} alt={title} />
+                    <div className="img_overlay">
+                      <Link to={id}>View Recipe</Link>
+                      <button onClick={() => console.log('add')}>
+                        Add to Grocery List
+                      </button>
+                      <button onClick={() => console.log('save')}>
+                        Save for Later
+                      </button>
+                    </div>
                   </div>
+                  <div>
+                    <h3>
+                      {title.length < maxTitleChars
+                        ? title
+                        : title.substr(0, maxTitleChars) + '...'}
+                    </h3>
+                  </div>
+                  <p>
+                    {description.length < maxDescChars
+                      ? description
+                      : description.substr(0, maxDescChars) + '...'}
+                  </p>
                 </div>
-                <h3>
-                  {title.length < maxTitleChars
-                    ? title
-                    : title.substr(0, maxTitleChars) + '...'}
-                </h3>
-                <p>
-                  {description.length < maxDescChars
-                    ? description
-                    : description.substr(0, maxDescChars) + '...'}
-                </p>
-              </div>
-            </RecipeCard>
-          );
-        })}
-      </Grid>
-    </Layout>
-  );
-};
+              </RecipeCard>
+            );
+          })}
+        </Grid>
+      </Layout>
+    );
+  }
+}
 
 export default Recipes;
 export const recipesQuery = graphql`
