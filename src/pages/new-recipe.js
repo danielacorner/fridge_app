f46a0b5a7b7dbfa043df8163365455f721bcdda8
dcorn068 <dcorn068@users.noreply.github.com>
2018-10-10 15:23:17 -0400

    debug react form re-renders by moving styled-components outside render function

--- src/pages/new-recipe.js
+++ src/pages/new-recipe.js
@@ -6,8 +6,95 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import CloseIcon from '@material-ui/icons/Close';
+const Header = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  align-items: end;
+  padding: 8px 40px 0 40px;
+  h1 {
+    width: 100%;
+    font-size: 36px;
+  }
+`;
 
+const Form = styled.form`
+  padding: 20px;
+  display: grid;
+  grid-gap: 10px;
+  width: 100%;
+  .numbersGrid {
+    display: grid;
+    grid-auto-flow: column;
+    justify-content: start;
+    grid-gap: 20px;
+  }
+  .numField {
+    display: inline-block;
+  }
+  h3 {
+    font-size: 24px;
+  }
+  .ingGrid,
+  .instGrid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr)) 130px;
+    align-items: start;
+    grid-column-gap: 20px;
+    button {
+      align-self: center;
+      margin-bottom: 20px;
+      background: lightpink;
+      &:hover {
+        background: #ffd7dd;
+      }
+    }
+  }
+  .addIngBtn,
+  .addInstBtn {
+    margin-top: 10px;
+    background: lightpink;
+    &:hover {
+      background: #ffd7dd;
+    }
+  }
+
+  .ingredientsGrid {
+    margin: 20px 0;
+  }
+
+  .instructionsGrid {
+    margin: 20px 0;
+    .instGrid {
+      span.step {
+        margin-bottom: -10px;
+        font-size: 18px;
+        font-weight: 500;
+      }
+      grid-template-columns: 1fr 130px;
+      align-items: center;
+      button {
+        align-self: center;
+        margin-bottom: -10px;
+      }
+    }
+  }
+
+  .submitDiv {
+    display: grid;
+    justify-items: end;
+    button {
+      background: lightpink;
+      &:hover {
+        background: #ffd7dd;
+      }
+    }
+  }
+`;
 class NewRecipe extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
   state = {
     templateKey: 'recipe-page',
     title: '',
@@ -23,10 +110,11 @@ class NewRecipe extends React.Component {
     tags: []
   };
 
-  handleChange = name => event => {
+  handleChange = event => {
     this.setState({
-      [name]: event.target.value
+      [event.target.name]: event.target.value
     });
+    console.table(this.state);
   };
 
   handleAddIngredient = () => {
@@ -40,10 +128,9 @@ class NewRecipe extends React.Component {
       quantity: ''
     };
     this.setState({
-      ingredients: [...this.state.ingredients, newIngredient]
+      ingredients: this.state.ingredients.concat(newIngredient)
     });
   };
-
   handleAddInstruction = () => {
     const newInstruction = {
       id:
@@ -68,98 +155,13 @@ class NewRecipe extends React.Component {
   handleRemoveInstruction = index => {
     this.setState({
       instructions: [
-        ...this.state.ingredients.slice(0, index),
-        ...this.state.ingredients.slice(index + 1)
+        ...this.state.instructions.slice(0, index),
+        ...this.state.instructions.slice(index + 1)
       ]
     });
   };
 
   render() {
-    const Header = styled.div`
-      display: grid;
-      grid-template-columns: 1fr;
-      align-items: end;
-      padding: 8px 40px 0 40px;
-      h1 {
-        width: 100%;
-        font-size: 36px;
-      }
-    `;
-
-    const Form = styled.form`
-      padding: 20px;
-      display: grid;
-      grid-gap: 10px;
-      width: 100%;
-      .numbersGrid {
-        display: grid;
-        grid-auto-flow: column;
-        justify-content: start;
-        grid-gap: 20px;
-      }
-      .numField {
-        display: inline-block;
-      }
-      h3 {
-        font-size: 24px;
-      }
-      .ingGrid,
-      .instGrid {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr)) 130px;
-        align-items: start;
-        grid-column-gap: 20px;
-        button {
-          align-self: center;
-          margin-bottom: 20px;
-          background: lightpink;
-          &:hover {
-            background: #ffd7dd;
-          }
-        }
-      }
-      .addIngBtn,
-      .addInstBtn {
-        margin-top: 10px;
-        background: lightpink;
-        &:hover {
-          background: #ffd7dd;
-        }
-      }
-
-      .ingredientsGrid {
-        margin: 20px 0;
-      }
-
-      .instructionsGrid {
-        margin: 20px 0;
-        .instGrid {
-          span.step {
-            margin-bottom: -10px;
-            font-size: 18px;
-            font-weight: 500;
-          }
-          grid-template-columns: 1fr 130px;
-          align-items: center;
-          button {
-            align-self: center;
-            margin-bottom: -10px;
-          }
-        }
-      }
-
-      .submitDiv {
-        display: grid;
-        justify-items: end;
-        button {
-          background: lightpink;
-          &:hover {
-            background: #ffd7dd;
-          }
-        }
-      }
-    `;
-
     return (
       <Layout>
         <Header>
@@ -173,12 +175,16 @@ class NewRecipe extends React.Component {
           netlify
           data-netlify="true"
           data-netlify-honeypot="bot-field"
+          key="form"
         >
           <TextField
+            key="titlefield"
             required
             label="Name"
+            name="title"
             className="textField"
-            onChange={() => this.handleChange('title')}
+            onChange={this.handleChange}
+            value={this.state.title}
             margin="normal"
             variant="outlined"
           />
@@ -186,7 +192,9 @@ class NewRecipe extends React.Component {
             required
             label="Image URL"
             className="textField"
-            onChange={() => this.handleChange('image')}
+            name="image"
+            onChange={this.handleChange}
+            value={this.state.image}
             margin="normal"
             variant="outlined"
           />
@@ -196,7 +204,8 @@ class NewRecipe extends React.Component {
               label="Servings"
               type="number"
               className="textField numField"
-              onChange={() => this.handleChange('serves')}
+              name="serves"
+              onChange={this.handleChange}
               margin="normal"
               variant="outlined"
             />
@@ -204,7 +213,8 @@ class NewRecipe extends React.Component {
               required
               label="Time (minutes)"
               type="number"
-              onChange={() => this.handleChange('minutes')}
+              name="minutes"
+              onChange={this.handleChange}
               className="textField numField"
               margin="normal"
               variant="outlined"
@@ -214,7 +224,8 @@ class NewRecipe extends React.Component {
             label="Description"
             multiline
             rowsMax="4"
-            onChange={() => this.handleChange('description')}
+            name="description"
+            onChange={this.handleChange}
             className="textField"
             margin="normal"
             helperText="Write a short description for your recipe"
@@ -224,12 +235,11 @@ class NewRecipe extends React.Component {
             <h3>Ingredients</h3>
             {this.state.ingredients.map((ing, index) => {
               return (
-                <div className="ingGrid" key={ing.id}>
+                <div className="ingGrid" key={'ingredient_' + ing.id}>
                   <TextField
                     label="Ingredient"
-                    onChange={() =>
-                      this.handleChange(`ingredients[${index}].ingredient`)
-                    }
+                    onChange={this.handleChange}
+                    name={`ingredients[${index}].ingredient`}
                     className="textField"
                     margin="normal"
                     helperText="Please use the singular form: onion, tomato, etc."
@@ -238,18 +248,16 @@ class NewRecipe extends React.Component {
                   <TextField
                     type="number"
                     label="Quantity"
-                    onChange={() =>
-                      this.handleChange(`ingredients[${index}].quantity`)
-                    }
+                    onChange={this.handleChange}
+                    name={`ingredients[${index}].quantity`}
                     className="textField"
                     margin="normal"
                     variant="outlined"
                   />
                   <TextField
                     label="Measure"
-                    onChange={() =>
-                      this.handleChange(`ingredients[${index}].measure`)
-                    }
+                    onChange={this.handleChange}
+                    name={`ingredients[${index}].measure`}
                     className="textField"
                     margin="normal"
                     helperText="unit: tbsp, tsp, cup, g, etc."
@@ -277,12 +285,12 @@ class NewRecipe extends React.Component {
             <h3>Instructions</h3>
             {this.state.instructions.map((inst, index) => {
               return (
-                <div className="instGrid" key={inst.id}>
+                <div className="instGrid" key={'instruction_' + inst.id}>
                   <TextField
                     label={`step ${index + 1}`}
-                    onChange={() =>
-                      this.handleChange(`instructions[${index}].instruction`)
-                    }
+                    onChange={this.handleChange}
+                    name={`instructions[${index}].instruction`}
+                    // defaultValue={this.state.instructions[index].instruction}
                     className="textField"
                     multiline
                     max-rows="6"
